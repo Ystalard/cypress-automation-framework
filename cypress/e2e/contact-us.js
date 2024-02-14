58f3737abfb43f08216877042d4849089132ccd6
Ystalard <nicolas.parisse.93@gmail.com>
2024-02-14 18:53:25 +0100

    Complete second challenge: test validate error on submission because all fields are required

--- cypress/e2e/contact-us.js
+++ cypress/e2e/contact-us.js
@@ -11,6 +11,12 @@ describe("Test Contact Us form via WebdriverUni", () => {
     });
 
     it("Should not be able to submit a successful submission via contact us form as all fields are required", () => {
-        //cyprus code
+        cy.visit("https://www.webdriveruniversity.com/Contact-Us/contactus.html")
+        cy.get('[name="first_name"]').type("Jean")
+        cy.get('[name="last_name"]').type("Valjean")
+        cy.get('[name="email"]').type("Jean.Valjean@email.com")
+        
+        cy.get('[type="submit"]').click()
+        cy.get('body').contains('Error: all fields are required')
     });
 })
