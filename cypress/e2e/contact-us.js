29d34e4814aeca2c762158aebfb1f3d9ce1158d4
Ystalard <nicolas.parisse.93@gmail.com>
2024-02-14 18:50:44 +0100

    Complete first challenge: test validate the submission

--- cypress/e2e/contact-us.js
+++ cypress/e2e/contact-us.js
@@ -2,12 +2,15 @@
 
 describe("Test Contact Us form via WebdriverUni", () => {
     it("Should be able to submit a successful submission via contact us form", () => {
-        //cypress code
-        cy.visit("https://www.webdriveruniversity.com/")
-        cy.get('#contact-us').click({force: true}) 
+        cy.visit("https://www.webdriveruniversity.com/Contact-Us/contactus.html")
+        cy.get('[name="first_name"]').type("Jean")
+        cy.get('[name="last_name"]').type("Valjean")
+        cy.get('[name="email"]').type("Jean.Valjean@email.com")
+        cy.get('textarea.feedback-input').type("I am a character from Les Miserables")
+        cy.get('[type="submit"]').click()
     });
 
     it("Should not be able to submit a successful submission via contact us form as all fields are required", () => {
-        //cypress code
+        //cyprus code
     });
 })
