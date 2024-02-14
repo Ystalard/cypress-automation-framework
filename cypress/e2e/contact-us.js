43a16de1d5e915f984536e8dd8a3830b58e75a0d
Ystalard <nicolas.parisse.93@gmail.com>
2024-02-14 18:55:19 +0100

    Force only one test through it.only

--- cypress/e2e/contact-us.js
+++ cypress/e2e/contact-us.js
@@ -10,7 +10,7 @@ describe("Test Contact Us form via WebdriverUni", () => {
         cy.get('[type="submit"]').click()
     });
 
-    it("Should not be able to submit a successful submission via contact us form as all fields are required", () => {
+    it.only("Should not be able to submit a successful submission via contact us form as all fields are required", () => {
         cy.visit("https://www.webdriveruniversity.com/Contact-Us/contactus.html")
         cy.get('[name="first_name"]').type("Jean")
         cy.get('[name="last_name"]').type("Valjean")
