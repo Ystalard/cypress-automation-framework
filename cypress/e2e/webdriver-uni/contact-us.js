e9df612eda147fa244c9a846a66a2f2db53502dc
Ystalard <nicolas.parisse.93@gmail.com>
2024-02-16 15:39:59 +0100

    assert title include the expected value

--- cypress/e2e/webdriver-uni/contact-us.js
+++ cypress/e2e/webdriver-uni/contact-us.js
@@ -4,6 +4,7 @@ describe("Test Contact Us form via WebdriverUni", () => {
     it("Should be able to submit a successful submission via contact us form", () => {
         cy.visit("https://www.webdriveruniversity.com/Contact-Us/contactus.html")
         cy.document().should('have.property', 'charset').and('eq', 'UTF-8')
+        cy.title().should('include', 'WebDriver | Contact Us')
         cy.get('[name="first_name"]').type("Jean")
         cy.get('[name="last_name"]').type("Valjean")
         cy.get('[name="email"]').type("Jean.Valjean@email.com")
