28b393658442ed011cf681cd8633ba69546415bb
Ystalard <nicolas.parisse.93@gmail.com>
2024-02-16 12:45:16 +0100

    Display different selectors samples

---
+++ cypress/e2e/webdriver-uni/selector.js
@@ -0,0 +1,33 @@
+describe("Selector examples", () => {
+    it("Examples of Selectors via WebdriverUni Contact Us Page", () => {
+        cy.visit("https://www.webdriveruniversity.com/Contact-Us/contactus.html");
+
+        // By Tag Name
+        cy.get('input')
+
+        //By attribute name and value
+        cy.get('input[name="first_name"]')
+
+        //By ID
+        cy.get('#contact_me')
+
+        //By Class
+        cy.get('.feedback-input')
+
+        //By multiple classes
+        cy.get('.navbar.navbar-inverse')
+
+        //By two different attributes
+        cy.get('input[name="first_name"][placeholder="First Name"]')
+
+        //By xpath
+        cy.xpath('//input[@name="first_name"]')
+
+        //By CSS
+        cy.get('input[name="first_name"][placeholder="First Name"]')
+
+        //By Text
+        cy.contains('CONTACT US')
+
+    })
+})
