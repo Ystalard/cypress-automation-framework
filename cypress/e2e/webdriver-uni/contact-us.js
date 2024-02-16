320599af64955587d71721ab87688056dd2ecc22
Ystalard <nicolas.parisse.93@gmail.com>
2024-02-16 15:43:16 +0100

    assert url of the page includes the expected value

--- cypress/e2e/webdriver-uni/contact-us.js
+++ cypress/e2e/webdriver-uni/contact-us.js
@@ -5,6 +5,7 @@ describe("Test Contact Us form via WebdriverUni", () => {
         cy.visit("https://www.webdriveruniversity.com/Contact-Us/contactus.html")
         cy.document().should('have.property', 'charset').and('eq', 'UTF-8')
         cy.title().should('include', 'WebDriver | Contact Us')
+        cy.url().should('include', 'Contact-Us/contactus.html')
         cy.get('[name="first_name"]').type("Jean")
         cy.get('[name="last_name"]').type("Valjean")
         cy.get('[name="email"]').type("Jean.Valjean@email.com")
