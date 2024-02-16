d52c577a0a098a5e50d4d6b4fe939885406fded8
Ystalard <nicolas.parisse.93@gmail.com>
2024-02-16 15:37:18 +0100

    extend should assertion with and

--- cypress/e2e/webdriver-uni/contact-us.js
+++ cypress/e2e/webdriver-uni/contact-us.js
@@ -3,6 +3,7 @@
 describe("Test Contact Us form via WebdriverUni", () => {
     it("Should be able to submit a successful submission via contact us form", () => {
         cy.visit("https://www.webdriveruniversity.com/Contact-Us/contactus.html")
+        cy.document().should('have.property', 'charset').and('eq', 'UTF-8')
         cy.get('[name="first_name"]').type("Jean")
         cy.get('[name="last_name"]').type("Valjean")
         cy.get('[name="email"]').type("Jean.Valjean@email.com")
