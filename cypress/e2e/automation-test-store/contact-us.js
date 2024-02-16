202e8681d79a40ac544e0ea4347c274485c8e50d
Ystalard <nicolas.parisse.93@gmail.com>
2024-02-16 12:22:13 +0100

    Improve contact us test for automation test store website with xpath

--- cypress/e2e/automation-test-store/contact-us.js
+++ cypress/e2e/automation-test-store/contact-us.js
@@ -2,20 +2,21 @@
 
 describe("Test Contact Us form via Automation Test Store", () => {
     it("Should be able to submit a successful submission via contact us form", () => {
-       cy.visit("https://automationteststore.com/"); 
-       cy.get('.info_links_footer > :nth-child(5) > a').click();
-       cy.get('#ContactUsFrm_first_name').type("Jean");
-       cy.get('#ContactUsFrm_email').type("Jean.Valjean@email.com");
-       cy.get('#ContactUsFrm_enquiry').type("Do you provide international shipping?");
-       cy.get('.col-md-6 > .btn').click();
+        cy.visit("https://automationteststore.com/"); 
+        cy.xpath('//a[contains(@href, "contact") and starts-with(text(), "Con")]').click();
+        cy.get('#ContactUsFrm_first_name').type("Jean");
+        cy.get('#ContactUsFrm_email').type("Jean.Valjean@email.com");
+        cy.get('#ContactUsFrm_enquiry').type("Do you provide international shipping?");
+        cy.get('.col-md-6 > .btn').click();
     })
 
     it("Should not be able to submit a successful submission via contact us form as all fields are required", () => {
         cy.visit("https://automationteststore.com/"); 
-       cy.get('.info_links_footer > :nth-child(5) > a').click();
-       cy.get('#ContactUsFrm_first_name').type("Jean");
-       cy.get('#ContactUsFrm_email').type("JeanValjean"); 
-       cy.get('#ContactUsFrm_enquiry').type("Do you provide international shipping?");
-       cy.get('.col-md-6 > .btn').click();
+        cy.xpath('//a[contains(@href, "contact") and starts-with(text(), "Con")]').click();
+        cy.get('#ContactUsFrm_first_name').type("Jean");
+        cy.get('#ContactUsFrm_email').type("JeanValjean"); 
+        cy.get('#ContactUsFrm_enquiry').type("Do you provide international shipping?");
+        cy.get('.col-md-6 > .btn').click();
+        cy.xpath('//*[contains(text(), "E-Mail Address does not appear to be valid!")]')
      })
 })  
