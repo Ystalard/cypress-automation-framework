00f4a02ea4f5f2824afe36fb51afc026557d67ec
Ystalard <nicolas.parisse.93@gmail.com>
2024-02-16 17:15:35 +0100

    Unexpected behaviour when executing non cypress command due to cypress being asynchronous

--- cypress/e2e/automation-test-store/contact-us.test.js
+++ cypress/e2e/automation-test-store/contact-us.test.js
@@ -1,7 +1,7 @@
 /// <reference types="cypress" />
 
 describe("Test Contact Us form via Automation Test Store", () => {
-    it("Should be able to submit a successful submission via contact us form", () => {
+    it.only("Should be able to submit a successful submission via contact us form", () => {
         cy.visit("https://automationteststore.com/"); 
         cy.xpath('//a[contains(@href, "contact") and starts-with(text(), "Con")]').click();
         cy.get('#ContactUsFrm_first_name').type("Jean");
@@ -10,6 +10,7 @@ describe("Test Contact Us form via Automation Test Store", () => {
         cy.get('#ContactUsFrm_enquiry').type("Do you provide international shipping?");
         cy.get('button[title="Submit"]').click();
         cy.get('.mb40 > :nth-child(3)').should('have.text', "Your enquiry has been successfully sent to the store owner!")
+        console.log("Test has been completed!")
     })
 
     it("Should not be able to submit a successful submission via contact us form as all fields are required", () => {
