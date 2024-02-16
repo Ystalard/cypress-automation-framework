0f7dd54f34125a6f765065b8d307083cb010ae44
Ystalard <nicolas.parisse.93@gmail.com>
2024-02-16 10:17:33 +0100

    Prevent XHR command log to slowdown the test process

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,10 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx,feature}"
+    specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx,feature}",
+    env: {
+      // set environment variables
+      hideXHRInCommandLog: "true"
+    },
   }
 });
