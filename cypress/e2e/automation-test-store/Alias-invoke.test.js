190b1ccdf9e8eaa2740021527e70f855f63e6002
Ystalard <nicolas.parisse.93@gmail.com>
2024-02-26 14:25:26 +0100

    Switched from hari care to skin care. Improved validation upon card and out of stock

--- cypress/e2e/automation-test-store/Alias-invoke.test.js
+++ cypress/e2e/automation-test-store/Alias-invoke.test.js
@@ -3,26 +3,32 @@
 describe('Alias and Invoke', () => {
     beforeEach(() => {
         cy.visit("https://automationteststore.com/")
-        cy.get("a[href*='product/category&path=']").contains('Hair Care').click()
+        cy.get("a[href*='product/category&path=']").contains('Skincare').click()
         cy.get("#grid").click()
         cy.get(".thumbnails.grid .thumbnail").as("productThumbnailGrid")
     })
 
-    it('Validate a specific hair care product', () => {
+    it('Validate a specific skin care product', () => {
         cy.visit("https://automationteststore.com/")
-        cy.get("a[href*='product/category&path=']").contains('Hair Care').click()
+        cy.get("a[href*='product/category&path=']").contains('Skincare').click()
         cy.get(".fixed_wrapper .prdocutname").eq(0).invoke('text').as('productThumbnail')
         cy.get('@productThumbnail').its('length').should('be.gt', 5)
-        cy.get('@productThumbnail').should('include', 'Seaweed Conditioner')
+        cy.get('@productThumbnail').should('include', 'Flash Bronzer Body Gel')
     })
 
     it('Validate product thumbnail count', () => {
-        cy.get('@productThumbnailGrid').should('have.length', 4)
+        cy.get('@productThumbnailGrid').should('have.length', 5)
     })
 
-    it('Validate a product can be added to cart', () => {
-        cy.get('@productThumbnailGrid').each(($el, index, $list) => {
-            cy.get($el).get('.productcart').invoke('attr', 'title').should('include', 'Add to Cart')
+    it.only('Validate a product can be added to cart', () => {
+        cy.get('@productThumbnailGrid').find('.productcart').each(($el, index, $list) => {
+                    cy.wrap($el).invoke('attr', 'title').should('include', 'Add to Cart')
         })
-    });
+    })
+
+    it.only('Validate a product is out of stock', () => {
+        cy.get('@productThumbnailGrid').find('.nostock').each($el => {
+            cy.wrap($el).should('have.text', 'Out of Stock')
+        })
+    })
 })
