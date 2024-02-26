517e7f15be508f78845d9564aff15db4cafe8b6e
Ystalard <nicolas.parisse.93@gmail.com>
2024-02-26 11:23:56 +0100

    Correct use of variable through promises

--- cypress/e2e/automation-test-store/variables-and-cy-command.test.js
+++ cypress/e2e/automation-test-store/variables-and-cy-command.test.js
@@ -9,4 +9,19 @@ describe('Verifying variables, cypress commands and jquery commands', () => {
         // makeupLink.click()
         cy.get("a[href*='product/category&path=']").contains('Makeup').click()
     })
+
+    it('Navigation to specific product pages', () => {
+        cy.visit("https://automationteststore.com/")
+        cy.get("a[href*='product/category&path=']").contains('Makeup').click()
+
+        //following code will fail
+        // const header = cy.get('h1 .maintext')
+        // cy.log(header.text())
+
+        cy.get('h1 .maintext').then(($headerText) => {
+            const headerText = $headerText.text()
+            cy.log("Found header text: " + headerText)
+            expect(headerText).is.eq('Makeup')
+        })
+    });
 })
