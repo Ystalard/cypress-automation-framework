703a143af671198eef9d846900a0dd96421fa60c
Ystalard <nicolas.parisse.93@gmail.com>
2024-03-01 16:16:29 +0100

    project Id for cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "wztwmc",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
